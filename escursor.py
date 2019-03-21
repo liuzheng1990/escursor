95be670709484da693759d3474d1d33c5d986790
Zheng <liuzheng900204@gmail.com>
2019-03-21 15:42:18 +0800

    3rd commit

---
+++ escursor.py
@@ -0,0 +1,85 @@
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import scan
+
+
+def get_total_hits(es_conn, index, doc_type, body):
+	body = body.copy()
+	if 'size' not in body: 
+		body['size'] = 0
+	res = es_conn.search(index=index, doc_type=doc_type, body=body)
+	return res['hits']['total']
+
+class ESCursor:
+
+	default_query_body = {'query': {"match_all": {}}}
+
+	def __init__(self, es_conn, index, doc_type="_doc", body=default_query_body, 
+											total_number=None, batch_size=10000):
+		self.es_conn = es_conn
+		self.index = index
+		self.doc_type = doc_type
+		self.body = body
+		# total_number is the maximum number of docs user wants. 
+		# If it's larger than the actual total number, use the actual total number instead.
+		actual_total_number = get_total_hits(es_conn, index, doc_type, body)
+		if total_number is None or total_number > actual_total_number:
+			total_number = actual_total_number 
+		self.total_number = total_number
+		if batch_size < total_number:
+			self.batch_size = batch_size # parameter to "size", when fetching each batch
+		else:
+			self.batch_size = total_number
+		self.offset = 0 # the parameter to "from" keyword, when fetching each batch
+		self.inbtach_idx = 0 # index in the current batch
+		self.current_batch_size = 0 # the actual batch size. May be smaller than "batch_size".
+
+	def __iter__(self):
+		"""
+		function to initialize the iteration. 
+		Set all parameters to its initial state, and fetch the first batch.
+		"""
+		self.offset = 0
+		self.inbtach_idx = 0
+		self.current_batch_size = 0
+		self._fetch_next_batch()
+		return self
+
+	def _fetch_next_batch(self):
+		self.offset += self.current_batch_size
+		if self.offset >= self.total_number:
+			raise StopIteration
+		body_batch = {**self.body, "from": self.offset, "size": self.batch_size}
+		res_data = self.es_conn.search(index=self.index, doc_type=self.doc_type, body=body_batch)
+		self.hits = res_data["hits"]["hits"]
+		self.current_batch_size = len(self.hits)
+		self.inbtach_idx = 0
+
+	def __next__(self):
+		if self.inbtach_idx >= self.current_batch_size:
+			self._fetch_next_batch()
+			if self.inbtach_idx == self.current_batch_size:
+				raise StopIteration
+		r = self.hits[self.inbtach_idx]
+		self.inbtach_idx += 1
+		return r
+
+
+
+def get_id_iter(es, index, doc_type): # get the _id of each document
+	query_body = {'query': {'match_all': {}}, "stored_fields": []}
+	it = scan(es, query=query_body, index=index, doc_type=doc_type)
+	for rec in it:
+		yield rec['_id']
+
+
+
+if __name__ == "__main__":
+
+	es = Elasticsearch()
+	body = ESCursor.default_query_body
+	body['stored_fields'] = []
+	es_cursor = ESCursor(es, "bank", "contact", body=body, total_number=200, batch_size=10)
+
+	for id in es_cursor:
+		print(id)
+
